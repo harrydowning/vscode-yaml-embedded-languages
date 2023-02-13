af7402d717896298cde84d5a458af39c295318a9
harrydowning <harry.downing17@gmail.com>
2023-02-13 10:17:34 +0000

    Changed to work with any language, not just source.*

--- script.py
+++ script.py
@@ -1,51 +1,49 @@
 import json, os
 
 SCOPE = "inline.langs"
-SOURCE_LANGS = [
-    "dosbatch",
-    "clojure",
-    "coffee",
-    "c",
-    "cpp",
-    "csharp",
-    "css",
-    "diff",
-    "dockerfile",
-    "fsharp",
-    "go",
-    "groovy",
-    "properties",
-    "java",
-    "js",
-    "json",
-    "latex",
-    "css.less",
-    "lua",
-    "makefile",
-    "objc",
-    "perl",
-    "perl.6",
-    "powershell",
-    "python",
-    "r",
-    "ruby",
-    "rust",
-    "css.scss",
-    "shaderlab",
-    "shell",
-    "slim",
-    "sql",
-    "swift",
-    "ts",
-    "tsx",
-    "tex",
-    "yaml"
-]
+LANGS = {
+    "c": "source.c",
+    "clojure": "source.clojure",
+    "coffee": "source.coffee",
+    "cpp": "source.cpp",
+    "csharp": "source.csharp",
+    "css": "source.css",
+    "diff": "source.diff",
+    "dockerfile": "source.dockerfile",
+    "dosbatch": "source.dosbatch",
+    "fsharp": "source.fsharp",
+    "go": "source.go",
+    "groovy": "source.groovy",
+    "html": "text.html.derivative",
+    "java": "source.java",
+    "js": "source.js",
+    "json": "source.json",
+    "latex": "text.tex",
+    "lua": "source.lua",
+    "makefile": "source.makefile",
+    "markdown": "text.html.markdown",
+    "objc": "source.objc",
+    "perl": "source.perl",
+    "powershell": "source.powershell",
+    "properties": "source.properties",
+    "python": "source.python",
+    "r": "source.r",
+    "ruby": "source.ruby",
+    "rust": "source.rust",
+    "shaderlab": "source.shaderlab",
+    "shell": "source.shell",
+    "slim": "source.slim",
+    "sql": "source.sql",
+    "swift": "source.swift",
+    "tex": "text.tex",
+    "ts": "source.ts",
+    "tsx": "source.tsx",
+    "xml": "text.xml",
+    "yaml": "source.yaml"
+}
 
-TEXT_LANGS = [] # TODO
-
-def get_embedded_languages(langs):
-    return {f"meta.embedded.inline.{lang}": f"{lang}" for lang in langs}
+def get_embedded_languages(names):
+    return {f"meta.embedded.inline.{name}": f"{name}" for name in names}
 
 package = {
   "name": "yaml-embedded-languages",
@@ -64,21 +62,21 @@ package = {
         "path": "./syntaxes/injection.json",
         "scopeName": SCOPE,
         "injectTo": ["source.yaml"],
-        "embeddedLanguages": get_embedded_languages(SOURCE_LANGS)
+        "embeddedLanguages": get_embedded_languages(LANGS.keys())
       }
     ]
   }
 }
 
-def get_patterns(langs):
+def get_patterns(names):
     return [{
-        "include": f"#{lang}-block-scalar"
-    } for lang in langs]
+        "include": f"#{name}-block-scalar"
+    } for name in names]
 
 def get_repository(langs):
     return {
-        f"{lang}-block-scalar": {
-            "begin": f"(?:(\\|)|(>))([1-9])?([-+])?[ \t]+(#[ \t]*{lang})[ \t]*\\n",
+        f"{name}-block-scalar": {
+            "begin": f"(?:(\\|)|(>))([1-9])?([-+])?[ \t]+(#[ \t]*{name})[ \t]*\\n",
             "beginCaptures": {
                 "1": {
                     "name": "keyword.control.flow.block-scalar.literal.yaml"
@@ -101,19 +99,19 @@ def get_repository(langs):
                 {
                     "begin": "^([ ]+)(?! )",
                     "end": "^(?!\\1|\\s*$)",
-                    "name": f"meta.embedded.inline.{lang}",
-                    "patterns": [{"include": f"source.{lang}"}]
+                    "name": f"meta.embedded.inline.{name}",
+                    "patterns": [{"include": f"{lang}"}]
                 }
             ]
         }
-    for lang in langs
+    for name, lang in langs.items()
     }
 
 injection = {
     "scopeName": SCOPE,
     "injectionSelector": "L:source.yaml",
-    "patterns": get_patterns(SOURCE_LANGS),
-    "repository": get_repository(SOURCE_LANGS)
+    "patterns": get_patterns(LANGS.keys()),
+    "repository": get_repository(LANGS)
 }
 
 def main():
