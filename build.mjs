76ced84229cc4bc0c067813a1a055a1aee82fc2d
harrydowning <harry.downing17@gmail.com>
2024-09-05 21:14:44 +0100

    auto env replacement and stats

--- build.mjs
+++ build.mjs
@@ -1,19 +1,47 @@
 // @ts-check
-
+import fs from "fs";
 import esbuild from "esbuild";
+import bytes from "bytes";
+import chalk from "chalk";
 
-await esbuild.build({
+const env = () => ({
+  name: "env",
+  setup: (build) => {
+    build.onLoad({ filter: /.*/ }, (args) => {
+      let src = fs.readFileSync(args.path, "utf8");
+      for (const match of src.matchAll(/\bprocess\.env\.(.+?)\b/g)) {
+        const name = match[1];
+        src = src.replace(
+          new RegExp(`\\bprocess\\.env\\.${name}\\b`, "g"),
+          JSON.stringify(process.env[name]) || "undefined",
+        );
+      }
+
+      return { contents: src, loader: "default" };
+    });
+  },
+});
+
+const result = await esbuild.build({
   entryPoints: ["dist/extension.js"],
   bundle: true,
   platform: "node",
   outfile: "dist/extension.js",
   allowOverwrite: true,
   minify: true,
+  metafile: true,
   alias: {
     "@package": "../package.json",
   },
   external: ["vscode", "../package.json"],
-  define: {
-    "process.env.PRE_RELEASE": JSON.stringify(process.env.PRE_RELEASE),
-  },
+  plugins: [env()],
 });
+
+const stats = {};
+
+for (const [output, value] of Object.entries(result.metafile.outputs)) {
+  stats[output] = value.bytes;
+  console.log(output, chalk.bold.green(bytes(value.bytes)));
+}
+
+fs.writeFileSync("stats.json", JSON.stringify(stats));
