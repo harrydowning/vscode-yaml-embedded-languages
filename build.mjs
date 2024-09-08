1612da73bca514f1a1c7655d1cac10b6b355cf39
Harry Downing <74541141+harrydowning@users.noreply.github.com>
2024-09-08 08:56:44 +0100

    chore: improve build script (#42)

--- build.mjs
+++ build.mjs
@@ -1,19 +1,29 @@
 // @ts-check
-
+import fs from "fs";
 import esbuild from "esbuild";
+import bytes from "bytes";
+import chalk from "chalk";
+import { autoEnv } from "esbuild-plugin-auto-env";
 
-await esbuild.build({
-  entryPoints: ["dist/extension.js"],
+const result = await esbuild.build({
+  entryPoints: ["src/extension.ts"],
   bundle: true,
   platform: "node",
   outfile: "dist/extension.js",
-  allowOverwrite: true,
   minify: true,
+  metafile: true,
   alias: {
     "@package": "../package.json",
   },
   external: ["vscode", "../package.json"],
-  define: {
-    "process.env.PRE_RELEASE": JSON.stringify(process.env.PRE_RELEASE),
-  },
+  plugins: [autoEnv({ filter: /src/ })],
 });
+
+const stats = {};
+
+for (const [output, value] of Object.entries(result.metafile.outputs)) {
+  stats[output] = value.bytes;
+  console.log(output, chalk.bold.green(bytes(value.bytes)));
+}
+
+fs.writeFileSync("stats.json", JSON.stringify(stats));
