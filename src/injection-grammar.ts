76e5ca9451022dcdeaeb773974abcbd46112e5b4
Harry Downing <74541141+harrydowning@users.noreply.github.com>
2024-09-14 12:30:24 +0100

    feat: Comment commands [pre-release] (#48)

--- src/injection-grammar.ts
+++ src/injection-grammar.ts
@@ -1,4 +1,5 @@
-import { Languages } from "./constants";
+import packageJson from "@package";
+import { Languages, PRE_RELEASE } from "./constants";
 import { Writable } from "./writable";
 
 export class InjectionGrammar extends Writable {
@@ -16,42 +17,87 @@ export class InjectionGrammar extends Writable {
 
   #getPatterns() {
     const entries = Object.entries(this.languages);
-    return entries.map(([id, { scopeName, stripIndent }]) => ({
-      begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?\\s+(#\\s*(?:${id})\\s*\\n)`,
-      beginCaptures: {
-        1: {
-          name: "keyword.control.flow.block-scalar.literal.yaml",
-        },
-        2: {
-          name: "keyword.control.flow.block-scalar.folded.yaml",
-        },
-        3: {
-          name: "constant.numeric.indentation-indicator.yaml",
-        },
-        4: {
-          name: "storage.modifier.chomping-indicator.yaml",
-        },
-        5: {
-          name: "entity.name.type.yaml",
-        },
-      },
-      end: "^(?=\\S)|(?!\\G)",
-      patterns: [
+    const patterns = [];
+
+    if (PRE_RELEASE) {
+      patterns.push(
         {
-          begin: "(?>^|\\G)([ ]+)(?! )",
-          end: "^(?!\\1|\\s*$)",
-          while: stripIndent ? "^$|\\1" : undefined,
-          name: `${this.embeddedScopeNamePrefix}.${id}`,
-          patterns: [{ include: scopeName }],
+          begin: `#\\s*${packageJson.name}\\s*$`,
+          beginCaptures: {
+            0: { name: "entity.name.type.yaml" },
+          },
+          patterns: [{ include: this.injectionScopeName }],
         },
-      ],
-    }));
+        ...entries.map(([id, { scopeName, stripIndent }]) => ({
+          begin: `#\\s*${packageJson.name}\\s*:\\s*${id}\\s*$`,
+          beginCaptures: {
+            0: { name: "entity.name.type.yaml" },
+          },
+          patterns: [
+            {
+              begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?(.*)\\s*$`,
+              beginCaptures: {
+                1: { name: "keyword.control.flow.block-scalar.literal.yaml" },
+                2: { name: "keyword.control.flow.block-scalar.folded.yaml" },
+                3: { name: "constant.numeric.indentation-indicator.yaml" },
+                4: { name: "storage.modifier.chomping-indicator.yaml" },
+                5: {
+                  patterns: [
+                    { include: `${this.injectionScopeName}#comment` },
+                    {
+                      match: ".+",
+                      name: "invalid.illegal.expected-comment-or-newline.yaml",
+                    },
+                  ],
+                },
+              },
+              end: "^(?=\\S)|(?!\\G)",
+              patterns: [
+                {
+                  begin: "(?>^|\\G)([ ]+)(?! )",
+                  end: "^(?!\\1|\\s*$)",
+                  while: stripIndent ? "^$|\\1" : undefined,
+                  name: `${this.embeddedScopeNamePrefix}.${id}`,
+                  patterns: [{ include: scopeName }],
+                },
+              ],
+            },
+            { include: this.injectionScopeName },
+          ],
+        })),
+      );
+    }
+
+    patterns.push(
+      ...entries.map(([id, { scopeName, stripIndent }]) => ({
+        begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?\\s+(#\\s*(?:${id})\\s*)$`,
+        beginCaptures: {
+          1: { name: "keyword.control.flow.block-scalar.literal.yaml" },
+          2: { name: "keyword.control.flow.block-scalar.folded.yaml" },
+          3: { name: "constant.numeric.indentation-indicator.yaml" },
+          4: { name: "storage.modifier.chomping-indicator.yaml" },
+          5: { name: "entity.name.type.yaml" },
+        },
+        end: "^(?=\\S)|(?!\\G)",
+        patterns: [
+          {
+            begin: "(?>^|\\G)([ ]+)(?! )",
+            end: "^(?!\\1|\\s*$)",
+            while: stripIndent ? "^$|\\1" : undefined,
+            name: `${this.embeddedScopeNamePrefix}.${id}`,
+            patterns: [{ include: scopeName }],
+          },
+        ],
+      })),
+    );
+
+    return patterns;
   }
 
   valueOf() {
     return {
       scopeName: this.scopeName,
-      injectionSelector: `L:${this.injectionScopeName} -comment`,
+      injectionSelector: `L:${this.injectionScopeName} -comment -string -meta.embedded`,
       patterns: this.#getPatterns(),
     };
   }
