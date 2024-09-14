adf2889038074a64b2e550453f5847ce871cdac5
harrydowning <harry.downing17@gmail.com>
2024-09-14 10:21:51 +0100

    initial implementation

--- src/injection-grammar.ts
+++ src/injection-grammar.ts
@@ -16,36 +16,90 @@ export class InjectionGrammar extends Writable {
 
   #getPatterns() {
     const entries = Object.entries(this.languages);
-    return entries.map(([id, { scopeName, stripIndent }]) => ({
-      begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?\\s+(#\\s*(?:${id})\\s*\\n)`,
-      beginCaptures: {
-        1: {
-          name: "keyword.control.flow.block-scalar.literal.yaml",
+    return [
+      ...entries.map(([id, { scopeName, stripIndent }]) => ({
+        begin: `^#\\s*yaml-embedded-languages\\s*:\\s*${id}$`,
+        while: `^(?!#\\s*yaml-embedded-languages\\s*:\\s*${id})`,
+        beginCaptures: {
+          "0": {
+            name: "entity.name.type.yaml",
+          },
         },
-        2: {
-          name: "keyword.control.flow.block-scalar.folded.yaml",
+        patterns: [
+          {
+            begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?(.*\n?)`,
+            beginCaptures: {
+              1: {
+                name: "keyword.control.flow.block-scalar.literal.yaml",
+              },
+              2: {
+                name: "keyword.control.flow.block-scalar.folded.yaml",
+              },
+              3: {
+                name: "constant.numeric.indentation-indicator.yaml",
+              },
+              4: {
+                name: "storage.modifier.chomping-indicator.yaml",
+              },
+              5: {
+                patterns: [
+                  {
+                    include: "source.yaml#comment",
+                  },
+                  {
+                    match: ".+",
+                    name: "invalid.illegal.expected-comment-or-newline.yaml",
+                  },
+                ],
+              },
+            },
+            end: "^(?=\\S)|(?!\\G)",
+            patterns: [
+              {
+                begin: "(?>^|\\G)([ ]+)(?! )",
+                end: "^(?!\\1|\\s*$)",
+                while: stripIndent ? "^$|\\1" : undefined,
+                name: `${this.embeddedScopeNamePrefix}.${id}`,
+                patterns: [{ include: scopeName }],
+              },
+            ],
+          },
+          {
+            include: this.injectionScopeName,
+          },
+        ],
+      })),
+      ...entries.map(([id, { scopeName, stripIndent }]) => ({
+        begin: `(?i)(?:(\\|)|(>))([1-9])?([-+])?\\s+(#\\s*(?:${id})\\s*\\n)`,
+        beginCaptures: {
+          1: {
+            name: "keyword.control.flow.block-scalar.literal.yaml",
+          },
+          2: {
+            name: "keyword.control.flow.block-scalar.folded.yaml",
+          },
+          3: {
+            name: "constant.numeric.indentation-indicator.yaml",
+          },
+          4: {
+            name: "storage.modifier.chomping-indicator.yaml",
+          },
+          5: {
+            name: "entity.name.type.yaml",
+          },
         },
-        3: {
-          name: "constant.numeric.indentation-indicator.yaml",
-        },
-        4: {
-          name: "storage.modifier.chomping-indicator.yaml",
-        },
-        5: {
-          name: "entity.name.type.yaml",
-        },
-      },
-      end: "^(?=\\S)|(?!\\G)",
-      patterns: [
-        {
-          begin: "(?>^|\\G)([ ]+)(?! )",
-          end: "^(?!\\1|\\s*$)",
-          while: stripIndent ? "^$|\\1" : undefined,
-          name: `${this.embeddedScopeNamePrefix}.${id}`,
-          patterns: [{ include: scopeName }],
-        },
-      ],
-    }));
+        end: "^(?=\\S)|(?!\\G)",
+        patterns: [
+          {
+            begin: "(?>^|\\G)([ ]+)(?! )",
+            end: "^(?!\\1|\\s*$)",
+            while: stripIndent ? "^$|\\1" : undefined,
+            name: `${this.embeddedScopeNamePrefix}.${id}`,
+            patterns: [{ include: scopeName }],
+          },
+        ],
+      })),
+    ];
   }
 
   valueOf() {
